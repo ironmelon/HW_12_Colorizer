942f5926cd6b7acf7c67127d39740248657b6a5a
Dynnikov Oleg <dinnikovoleg@gmail.com>
2018-07-30 21:21:03 +0300

    Initial xcode project

---
+++ HW_12_Colorizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW_12_Colorizer
+//
+//  Created by Oleg Dynnikov on 7/30/18.
+//  Copyright © 2018 Oleg Dynnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
